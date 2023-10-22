520e91f59668d7f61a8c5c11401ac46c6ab95b16
ram4ik <ram4ik@hotmail.com>
2023-10-22 14:01:22 +0300

    Initial Commit

---
+++ MasteringTipKit/MasteringTipKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasteringTipKitApp.swift
+//  MasteringTipKit
+//
+//  Created by admin on 10/22/23.
+//
+
+import SwiftUI
+
+@main
+struct MasteringTipKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
